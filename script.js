880a736b32a3a55d98c3afaaa1e2eabdc07e36b1
Gilvan Farias <63085957+GilvanFarias@users.noreply.github.com>
2021-02-12 19:27:58 -0300

    atualização do css e script

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let pulando = false;
+let GameOver = false;
+let posicao = 0;
+
+
 //adicionando a funcao pula
 function pula(event) {
   if (event.keyCode === 32) {
@@ -31,3 +39,37 @@ function pula() {
     }
   }, 20);
 }
+
+//funcao que cria cactus aleatoriamente e caso dino colida da gameover.
+function criaCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosicao = 1000;
+  let cactusAleatorios = Math.random() * 6000;
+
+  if (GameOver) return;
+
+  cactus.classList.add('cactus');
+  background.appendChild(cactus);
+  cactus.style.left = cactusPosicao + 'px';
+
+  let leftTimer = setInterval(() => {
+    if (cactusPosicao < -60) {
+      // Saiu da tela
+      clearInterval(leftTimer);
+      background.removeChild(cactus);
+    } else if (cactusPosicao > 0 && cactusPosicao < 60 && posicao < 60) {
+      // Game over
+      clearInterval(leftTimer);
+      GameOver = true;
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+    } else {
+      cactusPosicao -= 10;
+      cactus.style.left = cactusPosicao + 'px';
+    }
+  }, 20);
+
+  setTimeout(criaCactus, cactusAleatorios);
+}
+
+criaCactus();
+document.addEventListener('keyup', pula);
